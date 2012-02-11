b1478ca2d69118664d6a23d16a073ebc03793859
Dirk Krause <dirkk0@googlemail.com>
2012-02-11 18:03:07 +0100

    somewhat flexible navigation.

--- app.js
+++ app.js
@@ -29,7 +29,25 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', routes.index);
+// app.get('/', routes.index);
+
+app.get('/', function(req, res){
+ res.redirect('/home');
+});
+
+app.get('/home', function(req, res){
+  res.render('index', {
+    title: 'Express',
+    id: 'home'
+  });
+});
+
+app.get('/example1', function(req, res){
+  res.render('example1', {
+    title: 'Express',
+    id: 'example1'
+  });
+});
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
